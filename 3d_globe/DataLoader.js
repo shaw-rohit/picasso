8a8ef7f5a2ed5d342ad381be33e2002155188bc0
Kjell Zijlemaker <kjell.zijlemaker@live.nl>
2019-03-12 10:02:09 +0100

    Transfered new gallery paintings to 3d_globe

--- 3d_globe/DataLoader.js
+++ 3d_globe/DataLoader.js
@@ -48,7 +48,13 @@ var water = svgContainer.append("path")
 var g = svgContainer.append("g")//For map
 var gPins = svgContainer.append("g"); //For pins on map (new abstract layer)
 var gArrows = svgContainer.append("g"); // For arrows of migration
-
+var tooltip = d3.select("body").append("div")   
+    .attr("class", "tooltip")               
+    .style("opacity", 0)
+    .style("z-index", 1);
+svgContainer.call(zoom) //Use zoom
+var number_windows = 1; // Initial number of windows
+var number_details_painting = 0;
 
 var url = "http://enjalot.github.io/wwsd/data/world/world-110m.geojson";
 var data_url = "http://enjalot.github.io/wwsd/data/world/ne_50m_populated_places_simple.geojson";
@@ -497,31 +503,55 @@ function update_visuals(year, data, show, projection){
         var randomLat = 0;//Math.random();
            
         // insert filtered data into world map
+        // insert filtered data into world map
         gPins.selectAll(".pin")
             .data(clustered_data)
             .enter().append("circle", ".pin")
-            .on("mouseover",function(d){     
-                console.log(d)   
-                div.transition()        
+            .on("mouseover",function(cluster){  
+                tooltip.transition()        
                 .duration(200)      
-                .style("opacity", .9);      
-                div.text("There are a total of " + d.id.length + " paintings in the style: " + d.sub )
+                .style("opacity", .9)
+                .style("left", (d3.event.pageX +20) + "px")     
+                .style("top", (d3.event.pageY - 28) + "px")
+                .style("z-index", 1);
+                
+                tooltip.text("There are a total of " + cluster.id.length + " paintings in the style: " + cluster.sub )
                 .style("left", (d3.event.pageX) + "px")     
                 .style("top", (d3.event.pageY - 28) + "px")
+            
+                paintings_list = subset_paintings(cluster, data);
+            
+                // Change text size according to amount of paintings
+                if(paintings_list.length < 2){
+                    tooltip.style("width", "200px");
+                }
+                else if(parseInt.length < 4){
+                    tooltip.style("width", "400px");
+                }
+                else{
+                    tooltip.style("width", "800px");
+                }
+            
+                // Weird bug of not updating the images the first time
+                for(var i = 0; i < 2; i++){
+                    slides = add_paintings(paintings_list, ".tooltip");
+                }
+
             })
-            .on("mouseout", function(d) {       
-                div.transition()        
+            .on("mouseout", function() {  
+                tooltip.transition()        
                 .duration(500)      
-                .style("opacity", 0);   
+                .style("opacity", 0)
+                .style("z-index", -1);
             })
-            .on("click", function(d){
-                clicked();
-                //TODO: give transition and remove map SVG, go to new screen to show the paintings and its statistics
+            .on("click", function(cluster){
+                open_stats_painting(cluster, data, number_windows, "window");
+                number_windows += 1;
             })
          
           .attr("fill", function(d) {return color[show][d['sub']];})    
           .transition()
-          .attr("r", function(d) {return 2*d['id'].length;})   
+          .attr("r", function(d) {return 4*Math.log(d['id'].length);})   
           .style("opacity", opacity)
           .duration(400)
           .attr("transform", function(d) {
@@ -531,14 +561,235 @@ function update_visuals(year, data, show, projection){
             ]) + ")";
         });
 
+
         
       }
 };
 
-function clicked(d) {
-    zoom.scaleBy(svgContainer.transition().duration(500), 1.1);
+function painting_gallery(number_windows, div){
+    var newWindow =  d3.select("body").append("div")
+    .attr("class", "window")
+    .attr("id", div + number_windows)
+    .style("opacity", 0);
+    var x = d3.select("#" + div + number_windows).append("div")
+        .attr("class", "x")
+        .style("opacity", 0)
+        .style("pointer-events","visible");
+    windowgrab = d3.select("#" + div + number_windows).append("div")
+        .attr("class", "windowgrab")
+        .style("pointer-events", "visible");
+    var statistics = d3.select("#" + div + number_windows).append("div")
+        .attr("class", "statistics")
+        .style("opacity", 0)
+        .style("pointer-events","visible");
+    var slidewindow = d3.select("#" + div + number_windows).append("div") 
+        .attr("class", "slidewindow")
+        .attr("id", 'slidewindow' + number_windows)
+        .style("opacity", 0);
 
+    newWindow.transition()        
+        .duration(200)      
+        .style("opacity", .9)
+        .style("left", (d3.event.pageX / 2) + "px")     
+        .style("top", (d3.event.pageY - 200) + "px");
+
+    var rightresizer = d3.select("#" + div + number_windows).append("div")
+        .attr("windownumber", number_windows)
+        .attr("class", "rightresize")
+        .style("pointer-events","visible");
+    var downresizer = d3.select("#" + div + number_windows).append("div")
+        .attr("windownumber", number_windows)
+        .attr("class", "downresize")
+        .style("pointer-events","visible");
+
+
+    var moveWindow = d3.drag()
+        .on('drag', function(){
+            console.log("grab")
+            x = d3.event.x;
+            y = d3.event.y;
+            
+            newWindow.style("left", x + "px");
+            newWindow.style("top",  y + "px");
+        }); 
+
+         newWindow.call(moveWindow);
+
+    var windowResizeRight = d3.drag()
+        .on('drag', function(){
+            console.log("right")
+            x = d3.event.x;
+            y = d3.event.y;
+
+            x = Math.max(50, x);
+            y = Math.max(50, y);
+            
+            newWindow.style("width", x + "px");
+            newWindow.style("height", y + "px");
+        }); 
+        
+    var windowResizeDown = d3.drag()
+        .on('drag', function(){
+            console.log("down")
+            x = d3.event.x;
+            y = d3.event.y;
+
+            x = Math.max(50, x);
+            y = Math.max(50, y);
+            
+            newWindow.style("width", x + "px");
+            newWindow.style("height", y + "px");
+        });    
+    
+    rightresizer.call(windowResizeRight);
+    downresizer.call(windowResizeDown);
+
+    statistics.transition()
+        .duration(200)      
+        .style("opacity", 1)
+        .style("left", (newWindow.width + 20) + "px")
+        .style("top", (newWindow.height - 10) + "px");
+
+    slidewindow.transition()
+        .duration(200)      
+        .style("opacity", 1)
+        .style("left", (newWindow.width + 20) + "px")
+        .style("top", (newWindow.height - 10) + "px");
+
+
+
+    x.transition()
+        .duration(200)
+        .style("opacity", 1)
+        .style("left", (newWindow.width + 10) + "px")     
+        .style("top", 10 + "px");
+        
+    x.on("click", function(){
+        console.log(div);
+        if(div == "window"){
+            newWindow
+            .transition().duration(1000)
+            .style("opacity", 0).remove(); 
+            slides
+                .transition().duration(1000)
+                .style("opacity", 0);
+            }
+        else{
+            d3.select("#" + div + number_details_painting)
+                .transition().duration(1000)
+                .style("opacity", 0)
+                .remove();
+            d3.select("div.window")
+                .style("z-index", 1)
+                .transition().duration(1000)
+                .style("opacity", 0.9);
+        }
+        });
+
+    return statistics;
+}
+
+// When clicked, new window will open. All divs within this window is defined here
+function open_stats_painting(cluster, data, number_windows, div) {
+    
+    statistics = painting_gallery(number_windows, div);
+  
+        
+    // slidewindow.on("mouseover", svgContainer.on('.zoom', null), console.log("isfjlfesijl"))
+    // .on("mouseleave", 
+    //     console.log("iijfes"),
+    //     zoom = d3.zoom() // Init zoom again
+    //         .scaleExtent([1, 8])
+    //         .on("zoom", zoomed),
+    //     svgContainer.call(zoom)
+    // );
+
+    statistics.text("ifjofejoifesjoifesjfesoij")
+
+    var paintings = retreive_paintings(data, cluster.id);
+
+    // Weird bug of not updating the images the first time
+    for(var i = 0; i < 2; i++){
+        slides = add_paintings(paintings, "#slidewindow" + number_windows)
+    }
+    
+    slides.on("click", function(painting){
+        number_details_painting +=1;
+        d3.select("#" + div + number_windows)
+            .transition().duration(1000)
+            .style("opacity", 0)
+            .style("z-index", -1)
+        details_painting(painting);
+    });
+    
+   
+    
   }
+
+
+  function details_painting(painting, div){
+    
+    statistics = painting_gallery(number_details_painting, "details")
+    statistics.html("This painting was made by " + painting.artist_full_name + " in " + painting.date +  " and was named " + "'" + painting.artwork_name + "'" +
+                    "<br /> The painting was made in " + painting.city + ": " + painting.country +
+                    "<p /> <img src= " + painting.image_url + " width= '500px' height = '500px' ></img> ");
+    console.log(painting);
+  }
+
+//=========================================== Painting images START
+
+  // Add the paintings src inside image
+  function add_paintings(paintings_list, div){
+
+    var slides = d3.select(div)
+        .selectAll("img")
+        .data(paintings_list);
+
+    slides.enter()
+        .append("img") 
+        .attr("class", "slide"); 
+
+    slides.attr("src", function(d){ return d.image_url;})
+        .attr("style", "float:left")
+        .style("pointer-events","visible");;
+    
+    slides.style("opacity", 0) //start invisible
+        .transition().duration(500) //schedule a transition to last 1000ms
+        .delay(function(d,i){return i*500;})
+        .style("opacity", 1); 
+
+    return slides;
+  }
+
+
+  // Create subset of the paintings (is now 50 percent of total)
+ function subset_paintings(cluster, data){
+    var paintings_id = []; 
+    var subset_amount =  Math.floor(cluster.id.length / 100 * 50) // Take half the ID's
+     
+    // Transfer all paintings to new array
+     for(var i = 0; i < subset_amount; i++){
+         paintings_id.push(cluster.id[i]);
+     }
+
+     paintings_list = retreive_paintings(data, paintings_id);
+    
+    return paintings_list;
+ } 
+
+ // Filtering paintings to get the correct one
+function retreive_paintings(data,paintings_id){
+    var paintings_list = [];
+    paintings_id.forEach(function(id){
+        paintings_list.push(data.filter(function(d){
+            return d.omni_id === id;
+        })[0]);
+    });
+
+    return paintings_list;
+}
+
+//=========================================== Painting images END
 
 
 function cluster_data(data, show){
