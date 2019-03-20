e7f4a5249623c36f2230bac8127d495964d942a6
Oscar Ligthart <oscarligthart@gmail.com>
2019-03-20 16:19:37 +0100

    Migration flows no longer remain on the globe forever

--- 3d_globe/js/update_visuals.js
+++ 3d_globe/js/update_visuals.js
@@ -53,6 +53,8 @@ function update_visuals(year, data, show, projection){
     current_styles_set.forEach(v => current_styles.push(v));
 
     if (show_migration == true){
+        // remove all previous arrows
+        gArrows.selectAll("#arrow").remove()
         //var migration = retrieve_migration(filtered_data, show, 'baroque')
         selected_subs.forEach(function(element){
             console.log(element)
